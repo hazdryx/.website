7416260fecec3c369b38f74ba038be539981f639
Hazdryx <hazdryx@gmail.com>
2021-09-25 17:05:16 -0500

    Created a simple home page

---
+++ src/components/Banner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="banner">
+    <img :src="this.src" />
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: ['src']
+}
+</script>
+
+<style>
+  .banner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  .banner > img {
+    position: absolute;
+    object-fit: cover;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+  .banner > .content {
+    position: absolute;
+    z-index: 1;
+
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" height="384" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home" class="container">
+    <Banner :src="require('@/assets/banner.jpg')">
+      <div style="width: 38em">
+        <h1 style="font-size: 8em; text-align: center; margin-bottom: 0.8rem">Hazdryx</h1>
+        <p>
+          Welcome to my small corner of the internet, my name is a Chris (aka Hazdryx). I'm a C# and JavaScript software developer.
+          This website is still in the works, but feel free to check out my socials and GitHub!
+        </p>
+      </div>
+    </Banner>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Banner from '@/components/Banner.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Banner
   }
 }
 </script>
